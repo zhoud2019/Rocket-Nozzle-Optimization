a462f1b9628b63a2a331d430ab65442ea117bfdb
zhoud2019 <zhoud2019@gmail.com>
2026-09-20 18:06:51 -0400

    Remove comments from objective_thrust_mass function

--- MATLAB/functions/objective_thrust_mass.m
+++ MATLAB/functions/objective_thrust_mass.m
@@ -1,22 +1,22 @@
 function J = objective_thrust_mass(x)
 
-%% Design variables
+% Design variables
 
 Dt = x(1);
 De = x(2);
 Ld = x(3);
 
-%% Fixed geometry
+% Fixed geometry
 
 Lc = 30;
 t = 3;
 
-%% Calculate nozzle performance
+% Calculate nozzle performance
 
 [F, ~, ~, ~, ~, ~, ~, ~, nozzle_mass, ~] = ...
     nozzle_performance(Dt, De, Lc, Ld, t);
 
-%% Objective
+% Objective
 
 J = -F / nozzle_mass;
 
