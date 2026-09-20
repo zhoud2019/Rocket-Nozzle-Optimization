737522fa1c595e289e75bb84bf59a46c328835cf
zhoud2019 <zhoud2019@gmail.com>
2026-09-20 18:07:50 -0400

    Update comments for nozzle_performance function

--- MATLAB/functions/nozzle_performance.m
+++ MATLAB/functions/nozzle_performance.m
@@ -3,7 +3,7 @@ function [F, mdot, ve, Me, Pe, Te, At, Ae, nozzle_mass, V_wall, ...
     nozzle_performance(Dt, De, Lc, Ld, t)
 
 
-%%  OPERATING CONDITIONS
+%  OPERATING CONDITIONS
 
 
 Pc = 3e6;              % Chamber pressure [Pa]
@@ -12,12 +12,12 @@ gamma = 1.2;           % Specific heat ratio
 R = 355;               % Specific gas constant [J/(kg*K)]
 Pa = 101325;           % Ambient pressure [Pa]
 
-%% MATERIAL PROPERTIES
+% MATERIAL PROPERTIES
 
 
 rho_material = 8000;  % Material density [kg/m^3]
 
-%% NOZZLE EFFICIENCY
+% NOZZLE EFFICIENCY
 
 
 % Simplified constant nozzle efficiency.
@@ -30,7 +30,7 @@ rho_material = 8000;  % Material density [kg/m^3]
 
 eta = 0.95;
 
-%% CONVERT GEOMETRY FROM mm TO m
+% CONVERT GEOMETRY FROM mm TO m
 
 
 Dt_m = Dt / 1000;
@@ -39,39 +39,39 @@ Lc_m = Lc / 1000;
 Ld_m = Ld / 1000;
 t_m  = t / 1000;
 
-%% FIXED CHAMBER DIAMETER
+% FIXED CHAMBER DIAMETER
 
 
 Dc = 40;                % Chamber diameter [mm]
 Dc_m = Dc / 1000;
 
-%% FLOW AREAS
+% FLOW AREAS
 
 
 At = pi * Dt_m^2 / 4;
 Ae = pi * De_m^2 / 4;
 
-%% AREA RATIO
+% AREA RATIO
 
 
 AR = Ae / At;
 
-%% GEOMETRY CALCULATIONS
+% GEOMETRY CALCULATIONS
 
 
 Rc = Dc_m / 2;
 Rt = Dt_m / 2;
 Re = De_m / 2;
 
-%% Converging half-angle
+% Converging half-angle
 
 theta_c = atand((Rc - Rt) / Lc_m);
 
-%% Diverging half-angle
+% Diverging half-angle
 
 theta_d = atand((Re - Rt) / Ld_m);
 
-%% THROAT CONDITIONS
+% THROAT CONDITIONS
 
 Mt = 1;
 
@@ -91,7 +91,7 @@ a_t = sqrt(gamma * R * Tt);
 % Throat velocity
 Vt = Mt * a_t;
 
-%% EXIT MACH NUMBER
+% EXIT MACH NUMBER
 
 
 areaMach = @(M) ...
@@ -103,26 +103,26 @@ areaMach = @(M) ...
 % Supersonic solution
 Me = fzero(areaMach, 3);
 
-%% EXIT TEMPERATURE
+% EXIT TEMPERATURE
 
 
 Te = Tc / ...
     (1 + ((gamma - 1)/2) * Me^2);
 
-%% EXIT PRESSURE
+% EXIT PRESSURE
 
 
 Pe = Pc * ...
     (Te / Tc)^(gamma/(gamma - 1));
 
-%% Exit Density and Speed of Sound
+% Exit Density and Speed of Sound
 
 rho_e = Pe / (R * Te);
 
 a_e = sqrt(gamma * R * Te);
 
 
-%% EXIT VELOCITY
+% EXIT VELOCITY
 
 
 ve_ideal = sqrt( ...
@@ -132,7 +132,7 @@ ve_ideal = sqrt( ...
 % Apply simplified nozzle efficiency
 ve = sqrt(eta) * ve_ideal;
 
-%% MASS FLOW RATE
+% MASS FLOW RATE
 
 
 % Calculate mass flow from throat conditions:
@@ -144,7 +144,7 @@ ve = sqrt(eta) * ve_ideal;
 
 mdot = rho_t * At * Vt;
 
-%% THRUST
+% THRUST
 
 
 F_momentum = mdot * ve;
@@ -153,7 +153,7 @@ F_pressure = (Pe - Pa) * Ae;
 
 F = F_momentum + F_pressure;
 
-%% NOZZLE WALL MASS
+% NOZZLE WALL MASS
 
 
 % Inner radii after accounting for wall thickness
@@ -162,13 +162,13 @@ Rc_inner = Rc - t_m;
 Rt_inner = Rt - t_m;
 Re_inner = Re - t_m;
 
-%% Outer converging volume
+% Outer converging volume
 
 V_conv_outer = ...
     (pi * Lc_m / 3) * ...
     (Rc^2 + Rc*Rt + Rt^2);
 
-%% Inner converging volume
+% Inner converging volume
 
 V_conv_inner = ...
     (pi * Lc_m / 3) * ...
@@ -176,13 +176,13 @@ V_conv_inner = ...
      Rc_inner*Rt_inner + ...
      Rt_inner^2);
 
-%% Outer diverging volume
+% Outer diverging volume
 
 V_div_outer = ...
     (pi * Ld_m / 3) * ...
     (Re^2 + Re*Rt + Rt^2);
 
-%% Inner diverging volume
+% Inner diverging volume
 
 V_div_inner = ...
     (pi * Ld_m / 3) * ...
@@ -190,7 +190,7 @@ V_div_inner = ...
      Re_inner*Rt_inner + ...
      Rt_inner^2);
 
-%% Wall volumes
+% Wall volumes
 
 V_conv = V_conv_outer - V_conv_inner;
 
@@ -198,7 +198,7 @@ V_div = V_div_outer - V_div_inner;
 
 V_wall = V_conv + V_div;
 
-%% Nozzle mass
+% Nozzle mass
 
 nozzle_mass = rho_material * V_wall;
 
